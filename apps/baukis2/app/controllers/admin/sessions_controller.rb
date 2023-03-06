76507a647e666eee1ccd63eff94f0b972fb2493a
Ryzhon <pekozono@email.com>
2023-03-06 13:17:04 +0900

    add:職員のプロフィールcrud作成

--- apps/baukis2/app/controllers/admin/sessions_controller.rb
+++ apps/baukis2/app/controllers/admin/sessions_controller.rb
@@ -14,8 +14,6 @@ class Admin::SessionsController < Admin::Base
             administrator = 
             Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
         end
-        logger.debug(@form.password)
-
         # if staff_member
         if Admin::Authenticator.new(administrator).authenticate(@form.password)
             # session[:staff_member_id]=staff_member.id
@@ -23,19 +21,14 @@ class Admin::SessionsController < Admin::Base
             # redirect_to :staff_root
 
             if administrator.suspended?
-                logger.debug("---------aaa")
                 flash.now.alert = "アカウントが停止されています"
                 render action: "new"
             else
-                logger.debug("-----------bbb")
-
                 session[:administrator_id] = administrator.id
                 flash.notice = "ログインしました"
                 redirect_to :admin_root
             end
         else
-            logger.debug("--------------ccc")
-
             flash.now.alert = "メールアドレスまたはパスワードが正しくありません"
             render action: "new"
         end
